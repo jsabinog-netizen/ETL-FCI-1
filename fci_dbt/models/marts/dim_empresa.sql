3ff8cc0d75144e4e952a555baeb904663b32b95f
Jorge0929 <jorgeandresabino123@gmail.com>
2026-07-06 11:22:30 -0500

    fix(dbt): dim_empresa modify the case to tiene_asesoria_legal

--- fci_dbt/models/marts/dim_empresa.sql
+++ fci_dbt/models/marts/dim_empresa.sql
@@ -10,7 +10,7 @@ select
 
     -- banderitas: 1 si el servicio esta ejecutado, 0 si no
     case when estado_diagnostico = 'ejecutado' then 1 else 0 end as tiene_diagnostico,
-    case when estado_asesoria = 'ejecutado' then 1 else 0 end as tiene_asesoria,
+    case when estado_asesoria = 'ejecutado' then 1 else 0 end as tiene_asesoria_legal,
     case when estado_transferencia = 'ejecutado' then 1 else 0 end as tiene_transferencia,
     case when estado_sensibilizacion = 'ejecutado' then 1 else 0 end as tiene_sensibilizacion,
     -- pendiente añadir estado de vacanta o asesoria legal
