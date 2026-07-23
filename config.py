28ddc729bb15f2d424aecdc5b150129a2dd1831a
Jorge0929 <jorgeandresabino123@gmail.com>
2026-07-23 16:49:44 -0500

    feat(config): agregar MODULES_GIZ y proyecto giz en PROJECTS

--- config.py
+++ config.py
@@ -133,6 +133,105 @@ MODULES_COLSUBSIDIO = {
     ]
     
 }
-MODULES_CUIDARTE = {
+MODULES_GIZ = {
+    "Registro_Giz":[
+        "Cu_ntas_personas_est_n_a_tu_cargo" ,"Es_jefe_a_del_hogar", "Inscripci_n_Completada",
+        "Tiene_convalidados_sus_t_tulos_acad_micos", "Tiene_hijos", "Ciudad_de_nacimiento", 
+        "Connected_To__s", "Email", "Secondary_Email", "Created_By", "Departamento_de_residencia",
+        "Direcci_n_de_residencia", "Documento_de_identidad", "Edad", "Tag", "Etnia", 
+        "Fecha_de_nacimiento", "G_nero", "Gestor_operativo", "Modified_By", "Municipio_de_residencia",
+        "Nacionalidad", "Email_Opt_Out", "Name", "Observaci_n_Calidad", "Pa_s_de_nacimiento",
+        "Pa_s_en_el_que_lo_realiz", "Primer_Apellido", "Primer_Nombre", "Record_Image", "Owner",
+        "Se_reconoce_como_parte_de_la_poblaci_n_LGBTI_Q", "Segundo_apellido", "Segundo_Nombre", 
+        "Tel_fono_de_contacto", "Tiene_alguna_de_estas_responsabilidades_de_cuidado", 
+        "Tipo_de_discapacidad", "Tipo_de_documento", "Tipo_de_participante", 
+        "Ultimo_nivel_educativo_alcanzado", "Validaci_n_inscripci_n", "V_ctima_del_conflicto_armado",
+        "Whatsapp_de_contacto", "Zona_geogr_fica_de_residencia"
+    ],
+    "Orientaci_n_Giz":[
+        "Primer_Nombre", "Primer_Apellido", "Segundo_apellido", "Tipo_de_documento", "G_nero", 
+        "Tipo_de_participante", "Nacionalidad", "Municipio_de_residencia", "Departamento_de_residencia", 
+        "Zona_geogr_fica_de_residencia", "Etnia", "Tipo_de_discapacidad", "V_ctima_del_conflicto_armado",
+        "Se_reconoce_como_parte_de_la_poblaci_n_LGBTI_Q", "Es_jefe_a_del_hogar", "Tiene_hijos", 
+        "Tiene_alguna_de_estas_responsabilidades_de_cuidado", "Ultimo_nivel_educativo_alcanzado", 
+        "Pa_s_en_el_que_lo_realiz", "Tiene_convalidados_sus_t_tulos_acad_micos", "rea_de_formaci_n", 
+        "Cuenta_con_experiencia_laboral_formal", "ltima_experiencia_laboral_tiene_experiencia_la", 
+        "rea_de_desempe_o", "Tipo_de_vinculaci_n", "Perfil_ocupacional", "Fecha_de_orientaci_n",
+        "Orientador", "Orientaci_n_Completada", "Recomendado_para_una_vacante_de", "Ocupaci_n_Actual", 
+        "Presenta_barreras_de_pre_vinculaci_n", "Barreras_interna_de_previnculaci_n", 
+        "Barrera_externa_de_pre_vinculaci_n", "Estrategia_para_la_superaci_n_de_barreras", 
+        "Remisi_n_atenci_n_psic_social", "Tiene_medio_de_transporte", "Cu_ntas_personas_est_n_a_tu_cargo",
+        "Registro_Giz", "Owner", "Tag", "Connected_To__s", "Observaci_n_Calidad", "Validaci_n_Calidad", "Metas"
+    ],
+    "Intermediaci_n_Giz":[
+        "Desea_hacer_otra_intermediaci_n", "C_digo_de_la_vacante", "C_digo_de_la_vacante_2", 
+        "Connected_To__s", "Email", "Secondary_Email", "Created_By", "Empresa", "Empresa_2",
+        "Estado", "Estado_2", "Tag", "Fecha_de_intermediaci_n", "Fecha_de_intermediaci_n_2",
+        "Modified_By", "Nit_de_la_empresa", "Nit_de_la_empresa_2", "Email_Opt_Out", 
+        "Nombre_de_la_vacante", "Nombre_de_la_vacante_2", "N_mero_de_celular", "Name", 
+        "Perfil_ocupacional", "Primer_apellido", "Primer_nombre", "Record_Image", "Owner",
+        "Responsable_de_la_Intermediaci_n", "Responsable_de_la_intermediaci_n_2", "Segundo_apellido",
+        "Segundo_nombre", "Whatsapp_de_contacto"
+    ],
+    "Colocaci_n_Giz":[
+        "Adjuntar_certificado_laboral", "Campo_Calidad_de_la_data", "Campo_Ocupacional", 
+        "Cargo_en_la_empresa", "Colocaci_n_Completada", "Connected_To__s", "Email",
+        "Secondary_Email", "Created_By", "Encargado_Colocaci_n", "Es_un_empleo_verde",
+        "Tag", "Fecha_de_vinculaci_n", "Modified_By", "NIT_de_empresa_contratante_empleador", 
+        "Email_Opt_Out", "Name", "Nombre_de_empresa_contratante_empleador", "N_mero_de_celular", 
+        "Observaci_n", "Observaci_n_Calidad", "Primer_apellido", "Primer_nombre", "Record_Image",
+        "Owner", "Revisi_n_de_calidad", "Salario", "Sector_econ_mico_empresa_contratante_empleador", 
+        "Segundo_apellido", "Segundo_nombre", "Tipo_de_contrato", "Whatsapp_de_contacto"
+    ], 
+    "Empresa_Giz": [
+        "Connected_To__s", "Secondary_Email", "Email", "Created_By", "D_gito_de_verificaci_n",
+        "Tag", "Modified_By", "Name", "Email_Opt_Out", "Nombre_Contacto", "Nombre_de_la_empresa",
+        "Record_Image", "Owner", "Rut", "Sector_al_que_pertenece", 
+    ], 
+    "Vacantes_Giz":[
+        "Qu_curso_requiere", "Qu_idioma", "Requiere_alg_n_curso", "Requiere_licencia_de_carro",
+        "Requiere_licencia_de_moto", "Requiere_manejo_de_alg_n_idioma", 
+        "Ciudad_Municipio_de_la_vacante", "Name", "Connected_To__s", "Email", "Secondary_Email", 
+        "Correo_envio_hoja_de_vida", "Created_By", "Departamendo_de_la_vacante", 
+        "Descripci_n_del_salario", "D_gito_de_verificaci_n", "Tag", 
+        "Fecha_de_finalizaci_n_de_la_vacante", "Fecha_de_inicio_de_la_vacante", "Modified_By",
+        "Nit_de_la_empresa", "Email_Opt_Out", "Nombre_Contacto", "Nombre_de_la_empresa", 
+        "Nombre_de_la_vacante", "N_mero_de_puestos_de_trabajo", "Perfil_de_la_vacante", "Record_Image",
+        "Owner", "Sector_al_que_pertenece", "Tiempo_de_experiencia_meses", "Tipo_de_contrato"
+    ],
+    "Mitigaci_n_Giz":[
+        "Qu_servicio_recibi_para_superar_la_barrera", "Connected_To__s", "Email", "Secondary_Email",
+        "Created_By", "Departamento", "Descripci_n_de_mitigacion", "Enfrenta_alg_n_tipo_de_barrera",
+        "Entidad_Bancaria", "Es_micromitigaci_n", "Estado_de_mitigaci_n", "Tag", "Modified_By", 
+        "Municipio", "N_de_cuenta_Bancaria", "Email_Opt_Out", "Nombre_de_encargado", "Name", 
+        "Nombre_del_propietario_de_la_cuenta", "N_mero_de_celular", "Primer_apellido", 
+        "Primer_nombre", "Record_Image", "Owner", "Qu_valor_recibi_para_superar_la_barrera", 
+        "Segundo_apellido", "Segundo_nombre", "Seleccione_el_tipo_de_barrera_individual",
+        "Soporte_mitigaci_n", "Tipo_de_documento", "Tipo_de_mitigaci_n"
+    ],
+    "Calidad_Giz":[
+        "Colocaci_n", "Connected_To__s", "Email", "Secondary_Email", "Created_By", 
+        "Departamento_de_residencia", "Direcci_n_de_residencia", "Estado_de_calidad", 
+        "Tag","Etnia", "Fecha_de_nacimiento", "Fecha_de_revisi_n", "G_nero", "Intermediaci_n", 
+        "Modified_By", "Municipio_de_residencia", "Email_Opt_Out", "Name", "N_mero_de_tel_fono",
+        "Observaci_n_de_calidad", "Orientaci_n", "Primer_apellido", "Primer_nombre", "Record_Image",
+        "Owner", "Registro", "Segundo_apellido", "Segundo_nombre", "Tipo_de_discapacidad",
+        "Tipo_de_documento", "Tipo_de_participante", "Ultimo_nivel_educativo_alcanzado", 
+        "Url_documento_de_identidad"
+    ]
+}
 
+PROJECT_ID = "zoho-bq-pipeline-492116"  
+
+PROJECTS = {
+    "colsubsidio": {
+        "dataset_id": "colsubsidio_ruta_empresas",
+        "env_prefix": "ZOHO",       
+        "modules": MODULES_COLSUBSIDIO,
+    },
+    "giz": {
+        "dataset_id": "proyecto_giz",       
+        "env_prefix": "ZOHO_GIZ",        
+        "modules": MODULES_GIZ,          
+    },
 }
