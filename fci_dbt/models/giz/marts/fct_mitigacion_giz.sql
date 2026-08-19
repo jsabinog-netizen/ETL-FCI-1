a90d256eab220fcce341c3e8e5adaccf34c3df03
Jorge0929 <jorgeandresabino123@gmail.com>
2026-08-19 11:01:33 -0500

    feat(giz): modifie fct_mitigacion_giz and add tests

--- fci_dbt/models/giz/marts/fct_mitigacion_giz.sql
+++ fci_dbt/models/giz/marts/fct_mitigacion_giz.sql
@@ -1,2 +1,49 @@
-SELECT * FROM {{ ref('stg_mitigacion_giz') }}
+with mitigacion as (
+    select *
+    from {{ ref('stg_mitigacion_giz') }}
+),
+
+registro as (
+    select
+        documento,
+        genero,
+        tipo_participante,
+        departamento,
+        ciudad
+    from {{ ref('stg_registro_giz') }}
+),
+
+fct_mitigacion as (
+    select
+        -- identificación
+        m.id,
+        m.documento,
+        m.primer_nombre,
+        m.primer_apellido,
+
+        -- demográficos
+        r.genero,
+        r.tipo_participante,
+        r.departamento,
+        r.ciudad,
+
+        -- mitigación
+        m.tipo_mitigacion,
+        m.es_micromitigacion,
+        m.estado_mitigacion,
+        m.enfrenta_barrera,
+        m.tipo_barrera,
+        m.servicio_recibido,
+        m.valor_recibido,
+        m.encargado_mitigacion,
+
+        -- fechas
+        m.fecha_mitigacion,
+        m.modified_time
+
+    from mitigacion m
+    left join registro r on m.documento = r.documento
+)
+
+select * from fct_mitigacion
 
